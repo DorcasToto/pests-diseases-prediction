da68539fff97a2a7e65070833cad725c481eea8d
dorcas3 <dorcas@sendyit.com>
2021-06-16 07:49:25 +0300

    signup design module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../app/auth/login/login.component'
+import { SignupComponent } from '../app/auth/signup/signup.component'
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
