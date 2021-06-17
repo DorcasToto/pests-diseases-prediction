a14fe715961a6f8b774977f3155c696d3ea2dfec
dorcas3 <dorcas@sendyit.com>
2021-06-17 22:06:55 +0300

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  login_url:'https://tranquil-falls-15489.herokuapp.com/api/v1/auth/login',
+  signup_url:'https://tranquil-falls-15489.herokuapp.com/api/v1/auth/signup',
+  pest_disease_url:'https://tranquil-falls-15489.herokuapp.com/api/v1/pests-and-diseases/pests-and-diseases'
 };
