012b91498a01a0728a6dd1110826ee27e1cf52da
dorcas3 <dorcas@sendyit.com>
2021-06-17 13:30:11 +0300

    homepage design

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  username: any
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.username = localStorage.getItem('username')
   }
-
+logout(){
+  // localStorage.removeItem("accessToken");
+  localStorage.clear()
+  // localStorage.removeItem("accessToken");
+  this.router.navigate(['/login'])
+  
+}
 }
